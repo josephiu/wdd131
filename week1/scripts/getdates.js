be7bee109767afa07b9d998a751e43a0fb687955
josephiu <iu.joseph37@gmail.com>
2026-05-13 23:24:45 +0400

    week  -  homepage

---
+++ week1/scripts/getdates.js
@@ -0,0 +1,7 @@
+// let date = new Date();
+// let day = date.getDate();
+// let month = date.getMonth() + 1;
+// let year = date.getFullYear();
+
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
